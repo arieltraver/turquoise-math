681c457f87a46caf678a22bd815406251cc25c89
Ariel Traver <traverariel@users.noreply.github.com>
2020-01-20 14:15:10 -0500

    Update running
    * Only run tests if permutations.py is called directly
    * Documentation

--- Collatz.py
+++ Collatz.py
@@ -2,18 +2,17 @@ import math
 
 
 def collatz(n, counter):
-    #print(n)
+    """
+    Counts the terms in a collatz sequence for any number.
+    Once it hits a power of two, no more odds will appear, so it multiplies by log2 of the current term."""
     if int(math.log(n, 2)) == math.log(n, 2):
         print(f"Total is {counter + math.log(n, 2)}")
         return counter + math.log(n, 2)
     else:
         while n % 2 == 0:
             n /= 2
-            #print(n)
             counter += 1
-        #print("Jumped")
-        #print(3 * n + 1)
         collatz((3 * n + 1) / 2, counter + 2)
 
 
-collatz(837799,1)
+collatz(837799, 1)
