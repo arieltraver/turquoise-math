d197a6bbacb3d70ca74fe3247e9e41b1091b2e51
Ariel Traver <traverariel@users.noreply.github.com>
2020-01-20 14:10:33 -0500

    Update running
    * Only run tests if permutations.py is called directly
    * Documentation

--- solution.py
+++ solution.py
@@ -1,9 +1,10 @@
 def y(x):
+    """A function which finds acceptable pythagorean triplets with a sum of 1000."""
     return (1_000_000 - 2000 * x) / (2000 - 2 * x)
-"""
-"""
+
 
 def solve():
+    """This function prints 500 possibilities, only one will be whole numbers. """
     for i in range(0, 500):
         print(f"{y(i)} {i} ")
 
@@ -21,6 +22,9 @@ print(bar == 6)
 
 
 def round_better(number, decimal_places):
+    """A simple rounding function.
+    :param number: the un-rounded number
+    :param decimal_places: the desired number of post-point decimal places"""
     number *= pow(10, decimal_places)
     if decimal_places == 0:
         return int(number)
@@ -28,9 +32,7 @@ def round_better(number, decimal_places):
     return number / pow(10, decimal_places)
 
 
-# print("{:.0f}".format(round_better(5.222, 0)))
-
-print(round_better(5.999999999999, 0))
+print(round_better(5.999999999999, 3))
 
 output = "their names are {1} and {0}, but i like {0} better"
 print(output.format('ariels_solution', 'austins_solution'))
