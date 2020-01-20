cc0dd67b0a62c67c8e81f5fed04a539e77fee0a8
Ariel Traver <traverariel@users.noreply.github.com>
2020-01-20 14:13:05 -0500

    Update running
    * Only run tests if permutations.py is called directly
    * Documentation

--- triangular.py
+++ triangular.py
@@ -1,9 +1,14 @@
 def is_triangular(i):
+    """
+    Determines if a number is triangular.
+    Since all triangular numbers are sums of arithmetic series, they can be written as n(n + 1) / 2.
+    """
     k = int(pow(2 * i, 0.5))
     return k * (k + 1) == 2 * i
 
 
 def factor_count(non_square):
+    """Counts factors for numbers which aren't square."""
     total = 2
     for i in range(2, int(pow(non_square, 0.5)) + 1):
         if non_square % i == 0:
@@ -12,6 +17,7 @@ def factor_count(non_square):
 
 
 def max_factor_triangle(low, high):
+    """Finds the triangular number with the most factors in a given range. """
     max_factors = 2
     record_holder = low
     for i in range(low, high + 1):
